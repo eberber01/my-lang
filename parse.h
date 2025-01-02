cfea67c96a24c6758db41b9272fcbcb54c5600e5
eberber01 <eberber01@gmail.com>
2025-01-02 14:13:55 -0800

    move to src

--- parse.h
+++
@@ -1,9 +0,0 @@
-#ifndef PARSE_H
-#define PARSE_H
-#include "lex.h"
-
-struct AstNode* parse_expression(Token* tokens, int token_len, int* current_token);
-struct AstNode* parse_factor(Token* tokens, int token_len, int* current_token);
-struct AstNode* parse_term(Token* tokens, int token_len, int* current_token);
-struct AstNode* parse(Token* tokens, int token_len);
-#endif
