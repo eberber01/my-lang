7f3d58e8a41c50f00bd8a200c49b7322b0ce92c6
eberber01 <eberber01@gmail.com>
2025-01-20 14:41:34 -0800

    update tests

--- src/test/symtab.cpp
+++ src/test/symtab.cpp
@@ -1,8 +1,31 @@
 #include <gtest/gtest.h>
+extern "C"{
+  #include "../symtab.h"
+}
 
-TEST(MYLANG, SymbolTable) {
-  EXPECT_STRNE("hello", "world");
-  // Expect equality.
-  EXPECT_EQ(7 * 6, 42);
+
+TEST(SymbolTable, Add) {
+  SymTabEntry* entry;
+  SymTabEntry* ret; 
+  SymTab* table;
+
+  table = symtab_new();
+  entry = make_symtab_entry((char*)"int", INT,KEYWORD, NULL);
+  symtab_add(table, entry); 
+
+  ret = symtab_get(table,(char*)"int" );
+
+  EXPECT_STREQ(entry->key, ret->key);
+  EXPECT_EQ(entry->type, ret->type);
   
+}
+
+TEST(SymbolTable, Get){
+  SymTab* table;
+  SymTabEntry* entry;
+  table = symtab_new();
+
+  entry = symtab_get(table, (char *)"entry");
+  EXPECT_TRUE(entry == NULL);
+
 }
