c5dfce67fc60d44222b1e1c6d6b710b80640b4cf
eberber01 <eberber01@gmail.com>
2025-11-25 22:42:22 -0800

    format

--- src/asm.c
+++ src/asm.c
@@ -414,10 +414,10 @@ Register *eval_bin_exp(AstNode *node, RISCV *_asm)
         reg = alloc_register(_asm);
         tmp = alloc_register(_asm);
 
-        //temp = rhs < lhs
+        // temp = rhs < lhs
         fprintf(_asm->out, "\tslt %s, %s, %s\n", tmp->label, right->label, left->label);
 
-        //invert → lhs <= rhs
+        // invert → lhs <= rhs
         fprintf(_asm->out, "\txori %s, %s, %d\n", reg->label, tmp->label, 1);
 
         free_register(left);
@@ -436,10 +436,10 @@ Register *eval_bin_exp(AstNode *node, RISCV *_asm)
         reg = alloc_register(_asm);
         tmp = alloc_register(_asm);
 
-        //temp = lhs < rhs
+        // temp = lhs < rhs
         fprintf(_asm->out, "\tslt %s, %s, %s\n", tmp->label, left->label, right->label);
 
-        //invert → lhs >= rhs
+        // invert → lhs >= rhs
         fprintf(_asm->out, "\txori %s, %s, %d\n", reg->label, tmp->label, 1);
 
         free_register(left);
@@ -488,19 +488,19 @@ void gen_ret(AstNode *node, RISCV *_asm)
 {
     AstRet *ret;
     Register *reg;
-    StackFrame* frame;
+    StackFrame *frame;
 
     ret = (AstRet *)node->as;
     frame = ret->func->frame;
-    
+
     // return val
     reg = eval_asm(ret->expr, _asm);
 
     // move to return a0
     fprintf(_asm->out, "\tadd a0, %s, zero\n", reg->label);
 
-    //restore stack
-    if(frame->size > 0)
+    // restore stack
+    if (frame->size > 0)
         emit_sp_decrease(ret->func->frame->size, _asm);
 
     emit_return_from_jump(_asm);
