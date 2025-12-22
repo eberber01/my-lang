cb79c48e5e08743738cb4758c66da54dce639565
eberber01 <eberber01@gmail.com>
2025-12-22 13:13:52 -0800

    isolate logic

--- src/llvm/backend.c
+++ src/llvm/backend.c
@@ -1,6 +1,4 @@
-
-#include "mylang/hashmap.h"
-#include "mylang/lex.h"
+#include "backend.h"
 #include <inttypes.h>
 #include <llvm-c/Analysis.h>
 #include <llvm-c/BitWriter.h>
@@ -9,11 +7,116 @@
 #include <llvm-c/Target.h>
 #include <llvm-c/Types.h>
 #include <mylang/ast.h>
+#include <mylang/hashmap.h>
+#include <mylang/lex.h>
 #include <mylang/util.h>
 #include <stdio.h>
 #include <stdlib.h>
 
-LLVMValueRef llvm_eval(AstNode *node, LLVMValueRef llvm_func, LLVMModuleRef mod, LLVMContextRef ctx)
+LLVMValueRef llvm_eval_bin_exp(AstBinExp *bin_exp, LLVMValueRef llvm_func, LLVMContextRef ctx)
+{
+
+    LLVMOpcode op;
+    LLVMBasicBlockRef bb;
+    LLVMBuilderRef builder = LLVMCreateBuilderInContext(ctx);
+    LLVMValueRef left = llvm_eval(bin_exp->left, llvm_func, ctx);
+    LLVMValueRef right = llvm_eval(bin_exp->right, llvm_func, ctx);
+
+    bb = LLVMAppendBasicBlockInContext(ctx, llvm_func, "binop");
+    LLVMPositionBuilderAtEnd(builder, bb);
+
+    switch (bin_exp->op_type)
+    {
+    case TOK_ADD:
+        op = LLVMAdd;
+        break;
+    case TOK_SUB:
+        op = LLVMSub;
+        break;
+    case TOK_MULT:
+        op = LLVMMul;
+        break;
+    case TOK_DIV:
+        op = LLVMSDiv;
+        break;
+    case TOK_EQUAL:
+        return LLVMBuildICmp(builder, LLVMIntEQ, left, right, "equal");
+    case TOK_NOT_EQUAL:
+        return LLVMBuildICmp(builder, LLVMIntNE, left, right, "not equal");
+    case TOK_LT:
+        return LLVMBuildICmp(builder, LLVMIntULT, left, right, "less than");
+    case TOK_LT_EQ:
+        return LLVMBuildICmp(builder, LLVMIntULE, left, right, "less than equal");
+    case TOK_GT:
+        return LLVMBuildICmp(builder, LLVMIntUGT, left, right, "greater than");
+    case TOK_GT_EQ:
+        return LLVMBuildICmp(builder, LLVMIntUGE, left, right, "greater than equal");
+    default:
+        perror("LLVM op type not implemented");
+        exit(1);
+    }
+    return LLVMBuildBinOp(builder, op, left, right, "binop");
+}
+
+LLVMValueRef llvm_eval_func_call(AstFuncCall *func_call, LLVMValueRef llvm_func, LLVMContextRef ctx)
+{
+
+    AstNode *n;
+    LLVMBasicBlockRef bb;
+    LLVMBuilderRef builder = LLVMCreateBuilderInContext(ctx);
+    LLVMValueRef *args = NULL;
+    size_t arg_length = func_call->args->length;
+    if (arg_length > 0)
+    {
+
+        args = (LLVMValueRef *)my_malloc(sizeof(LLVMValueRef) * arg_length);
+
+        for (size_t i = 0; i < arg_length; i++)
+        {
+            n = (AstNode *)vector_get(func_call->args, i);
+            args[i] = llvm_eval(n, llvm_func, ctx);
+        }
+    }
+
+    bb = LLVMAppendBasicBlockInContext(ctx, llvm_func, "fcall");
+    LLVMPositionBuilderAtEnd(builder, bb);
+
+    return LLVMBuildCall2(builder, func_call->symbol->llvm_type_ref, func_call->symbol->llvm_value_ref, args,
+                          arg_length, "");
+}
+
+LLVMValueRef llvm_eval_var_asgn(AstVarAsgn *var_asgn, LLVMValueRef llvm_func, LLVMContextRef ctx)
+{
+
+    LLVMBuilderRef builder = LLVMCreateBuilderInContext(ctx);
+    LLVMBasicBlockRef bb = LLVMAppendBasicBlockInContext(ctx, llvm_func, "asgn");
+    LLVMValueRef lval = llvm_eval(var_asgn->lval, llvm_func, ctx);
+    LLVMValueRef rval = llvm_eval(var_asgn->rval, llvm_func, ctx);
+    LLVMPositionBuilderAtEnd(builder, bb);
+    return LLVMBuildStore(builder, rval, lval);
+}
+
+LLVMValueRef llvm_eval_lval(AstLValue *lvalue)
+{
+    AstIdent *lval_ident;
+
+    switch (lvalue->kind)
+    {
+    case AST_LVAL_IDENT:
+        lval_ident = (AstIdent *)(lvalue->u.ident->as);
+        return lval_ident->symbol->llvm_value_ref;
+    default:
+        perror("LLVM eval lvalue not implemented");
+        exit(1);
+    }
+}
+
+LLVMValueRef llvm_eval_int_const(AstIntConst *int_const)
+{
+    return LLVMConstInt(LLVMInt32Type(), int_const->value, false);
+}
+
+LLVMValueRef llvm_eval(AstNode *node, LLVMValueRef llvm_func, LLVMContextRef ctx)
 {
 
     AstIdent *ident;
@@ -22,10 +125,7 @@ LLVMValueRef llvm_eval(AstNode *node, LLVMValueRef llvm_func, LLVMModuleRef mod,
     AstFuncCall *func_call;
     AstVarAsgn *var_asgn;
     AstLValue *lvalue;
-    AstIdent *lval_ident;
 
-    LLVMBasicBlockRef bb;
-    LLVMBuilderRef builder = LLVMCreateBuilderInContext(ctx);
     switch (node->type)
     {
     case AST_IDENT:
@@ -34,96 +134,102 @@ LLVMValueRef llvm_eval(AstNode *node, LLVMValueRef llvm_func, LLVMModuleRef mod,
         break;
     case AST_BIN_EXP:
         bin_exp = (AstBinExp *)node->as;
-        LLVMOpcode op;
-        LLVMValueRef left = llvm_eval(bin_exp->left, llvm_func, mod, ctx);
-        LLVMValueRef right = llvm_eval(bin_exp->right, llvm_func, mod, ctx);
-
-        bb = LLVMAppendBasicBlockInContext(ctx, llvm_func, "binop");
-        LLVMPositionBuilderAtEnd(builder, bb);
-
-        switch (bin_exp->op_type)
-        {
-        case TOK_ADD:
-            op = LLVMAdd;
-            break;
-        case TOK_SUB:
-            op = LLVMSub;
-            break;
-        case TOK_MULT:
-            op = LLVMMul;
-            break;
-        case TOK_DIV:
-            op = LLVMSDiv;
-            break;
-        case TOK_EQUAL:
-            return LLVMBuildICmp(builder,  LLVMIntEQ,  left,  right, "equal");
-        case TOK_NOT_EQUAL:
-            return LLVMBuildICmp(builder,  LLVMIntNE,  left,  right, "not equal");
-        case TOK_LT:
-            return LLVMBuildICmp(builder,  LLVMIntULT,  left,  right, "less than");
-        case TOK_LT_EQ:
-            return LLVMBuildICmp(builder,  LLVMIntULE,  left,  right, "less than equal");
-        case TOK_GT:
-            return LLVMBuildICmp(builder,  LLVMIntUGT,  left,  right, "greater than");
-        case TOK_GT_EQ:
-            return LLVMBuildICmp(builder,  LLVMIntUGE,  left,  right, "greater than equal");
-        default:
-            perror("LLVM op type not implemented");
-            exit(1);
-        }
-        return LLVMBuildBinOp(builder, op, left, right, "binop");
+        return llvm_eval_bin_exp(bin_exp, llvm_func, ctx);
     case AST_INT_CONST:
         int_const = (AstIntConst *)node->as;
-        return LLVMConstInt(LLVMInt32Type(), int_const->value, false);
+        return llvm_eval_int_const(int_const);
     case AST_FUNC_CALL:
         func_call = (AstFuncCall *)node->as;
-
-        AstNode *n;
-        LLVMValueRef *args = NULL;
-        size_t arg_length = func_call->args->length;
-        if (arg_length > 0)
-        {
-
-            args = (LLVMValueRef *)my_malloc(sizeof(LLVMValueRef) * arg_length);
-
-            for (size_t i = 0; i < arg_length; i++)
-            {
-                n = (AstNode *)vector_get(func_call->args, i);
-                args[i] = llvm_eval(n, llvm_func, mod, ctx);
-            }
-        }
-
-        bb = LLVMAppendBasicBlockInContext(ctx, llvm_func, "fcall");
-        LLVMPositionBuilderAtEnd(builder, bb);
-
-        return LLVMBuildCall2(builder, func_call->symbol->llvm_type_ref, func_call->symbol->llvm_value_ref, args,
-                              arg_length, "");
-
+        return llvm_eval_func_call(func_call, llvm_func, ctx);
     case AST_LVAL:
         lvalue = (AstLValue *)node->as;
-        switch (lvalue->kind)
-        {
-        case AST_LVAL_IDENT:
-            lval_ident = (AstIdent *)(lvalue->u.ident->as);
-            return lval_ident->symbol->llvm_value_ref;
-        default:
-            perror("LLVM eval lvalue not implemented");
-            exit(1);
-        }
+        return llvm_eval_lval(lvalue);
     case AST_VAR_ASGN:
-        var_asgn = (AstVarAsgn*)node->as;
-        bb = LLVMAppendBasicBlockInContext(ctx, llvm_func, "asgn");
-        LLVMValueRef lval = llvm_eval(var_asgn->lval,  llvm_func,  mod,  ctx);
-        LLVMValueRef rval = llvm_eval(var_asgn->rval,  llvm_func,  mod,  ctx);
-        LLVMPositionBuilderAtEnd(builder, bb);
-        return LLVMBuildStore(builder, rval, lval);
+        var_asgn = (AstVarAsgn *)node->as;
+        return llvm_eval_var_asgn(var_asgn, llvm_func, ctx);
     default:
         perror("LLVM eval not implemented");
         exit(1);
     }
 }
 
-void _llvm_code_gen(AstNode *node, LLVMValueRef llvm_func, LLVMModuleRef mod, LLVMContextRef ctx)
+void llvm_gen_if(AstIfElse *if_stmt, LLVMValueRef llvm_func, LLVMContextRef ctx)
+{
+
+    LLVMBuilderRef builder = LLVMCreateBuilderInContext(ctx);
+    LLVMBasicBlockRef cond_block = LLVMCreateBasicBlockInContext(ctx, "cond");
+    LLVMBasicBlockRef then_block = LLVMCreateBasicBlockInContext(ctx, "then");
+    LLVMBasicBlockRef else_block = LLVMCreateBasicBlockInContext(ctx, "else");
+    LLVMBasicBlockRef cont_block = LLVMCreateBasicBlockInContext(ctx, "ifcont");
+
+    // If
+    LLVMValueRef cond = llvm_eval(if_stmt->if_expr, llvm_func, ctx);
+
+    LLVMAppendExistingBasicBlock(llvm_func, cond_block);
+    LLVMPositionBuilderAtEnd(builder, cond_block);
+    LLVMBuildCondBr(builder, cond, then_block, else_block);
+
+    // then
+    LLVMPositionBuilderAtEnd(builder, then_block);
+    LLVMAppendExistingBasicBlock(llvm_func, then_block);
+    _llvm_code_gen(if_stmt->if_body, llvm_func, ctx);
+    LLVMBuildBr(builder, cont_block);
+
+    // Else
+    if (if_stmt->else_body != NULL)
+    {
+        LLVMPositionBuilderAtEnd(builder, else_block);
+        LLVMAppendExistingBasicBlock(llvm_func, else_block);
+        _llvm_code_gen(if_stmt->else_body, llvm_func, ctx);
+        LLVMBuildBr(builder, cont_block);
+    }
+
+    LLVMAppendExistingBasicBlock(llvm_func, cont_block);
+}
+
+void llvm_gen_ret(AstRet *ret, LLVMValueRef llvm_func, LLVMContextRef ctx)
+{
+
+    LLVMBuilderRef builder = LLVMCreateBuilderInContext(ctx);
+    LLVMValueRef ret_val = llvm_eval(ret->expr, llvm_func, ctx);
+
+    LLVMBasicBlockRef bb = LLVMAppendBasicBlockInContext(ctx, llvm_func, "ret");
+    LLVMPositionBuilderAtEnd(builder, bb);
+
+    LLVMBuildRet(builder, ret_val);
+}
+
+void llvm_gen_var_dec(AstVarDec *dec, LLVMValueRef llvm_func, LLVMContextRef ctx)
+{
+
+    LLVMBuilderRef builder = LLVMCreateBuilderInContext(ctx);
+    LLVMBasicBlockRef bb = LLVMAppendBasicBlockInContext(ctx, llvm_func, "var");
+    LLVMPositionBuilderAtEnd(builder, bb);
+
+    LLVMTypeRef type_ref = LLVMInt32Type();
+    LLVMValueRef ref = LLVMBuildAlloca(builder, type_ref, dec->value);
+    dec->symbol->llvm_value_ref = ref;
+    dec->symbol->llvm_type_ref = type_ref;
+}
+
+void llvm_gen_var_def(AstVarDef *var_def, LLVMValueRef llvm_func, LLVMContextRef ctx)
+{
+
+    LLVMBuilderRef builder = LLVMCreateBuilderInContext(ctx);
+    LLVMValueRef init_val = llvm_eval(var_def->expr, llvm_func, ctx);
+
+    LLVMBasicBlockRef bb = LLVMAppendBasicBlockInContext(ctx, llvm_func, "var def");
+    LLVMPositionBuilderAtEnd(builder, bb);
+
+    LLVMTypeRef var_type_ref = LLVMInt32Type();
+    LLVMValueRef var_def_ref = LLVMBuildAlloca(builder, var_type_ref, var_def->value);
+    LLVMBuildStore(builder, init_val, var_def_ref);
+
+    var_def->symbol->llvm_value_ref = var_def_ref;
+    var_def->symbol->llvm_type_ref = var_type_ref;
+}
+
+void _llvm_code_gen(AstNode *node, LLVMValueRef llvm_func, LLVMContextRef ctx)
 {
 
     AstCompStmt *comp_stmt;
@@ -138,87 +244,33 @@ void _llvm_code_gen(AstNode *node, LLVMValueRef llvm_func, LLVMModuleRef mod, LL
     // AstFor *f_stmt;
     // AstUnaryExpr *unary_expr;
 
-    LLVMBasicBlockRef bb;
-    LLVMBuilderRef builder = LLVMCreateBuilderInContext(ctx);
     switch (node->type)
     {
     case AST_COMP_STMT:
         comp_stmt = (AstCompStmt *)node->as;
         for (size_t i = 0; i < comp_stmt->body->length; i++)
-            _llvm_code_gen((AstNode *)vector_get(comp_stmt->body, i), llvm_func, mod, ctx);
+            _llvm_code_gen((AstNode *)vector_get(comp_stmt->body, i), llvm_func, ctx);
         break;
     case AST_RET:
         ret = (AstRet *)node->as;
-
-        LLVMValueRef ret_val = llvm_eval(ret->expr, llvm_func, mod, ctx);
-
-        bb = LLVMAppendBasicBlockInContext(ctx, llvm_func, "ret");
-        LLVMPositionBuilderAtEnd(builder, bb);
-
-        LLVMBuildRet(builder, ret_val);
+        llvm_gen_ret(ret, llvm_func, ctx);
         break;
     case AST_VAR_DEC:
         dec = (AstVarDec *)node->as;
-        bb = LLVMAppendBasicBlockInContext(ctx, llvm_func, "var");
-        LLVMPositionBuilderAtEnd(builder, bb);
-
-        LLVMTypeRef type_ref = LLVMInt32Type();
-        LLVMValueRef ref = LLVMBuildAlloca(builder, type_ref, dec->value);
-        dec->symbol->llvm_value_ref = ref;
-        dec->symbol->llvm_type_ref = type_ref;
+        llvm_gen_var_dec(dec, llvm_func, ctx);
         break;
     case AST_VAR_DEF:
         var_def = (AstVarDef *)node->as;
-
-        LLVMValueRef init_val = llvm_eval(var_def->expr, llvm_func, mod, ctx);
-
-        bb = LLVMAppendBasicBlockInContext(ctx, llvm_func, "var def");
-        LLVMPositionBuilderAtEnd(builder, bb);
-
-        LLVMTypeRef var_type_ref = LLVMInt32Type();
-        LLVMValueRef var_def_ref = LLVMBuildAlloca(builder, var_type_ref, var_def->value);
-        LLVMBuildStore(builder, init_val, var_def_ref);
-
-        var_def->symbol->llvm_value_ref = var_def_ref;
-        var_def->symbol->llvm_type_ref = var_type_ref;
+        llvm_gen_var_def(var_def, llvm_func, ctx);
         break;
     case AST_EXPR_STMT:
         expr_stmt = (AstExprStmt *)node->as;
         if (expr_stmt->expr->type != AST_EMPTY_EXPR)
-            llvm_eval(expr_stmt->expr,  llvm_func,  mod,  ctx);
+            llvm_eval(expr_stmt->expr, llvm_func, ctx);
         break;
     case AST_IF:
-        if_stmt = (AstIfElse*)node->as;
-        LLVMBasicBlockRef cond_block = LLVMCreateBasicBlockInContext(ctx, "cond");
-        LLVMBasicBlockRef then_block = LLVMCreateBasicBlockInContext(ctx, "then");
-        LLVMBasicBlockRef else_block = LLVMCreateBasicBlockInContext(ctx, "else");
-        LLVMBasicBlockRef cont_block = LLVMCreateBasicBlockInContext(ctx, "ifcont");
-
-        //If
-
-
-        LLVMValueRef cond = llvm_eval(if_stmt->if_expr,  llvm_func,  mod,  ctx);
-
-        LLVMAppendExistingBasicBlock(llvm_func,  cond_block);
-        LLVMPositionBuilderAtEnd(builder,  cond_block);
-        LLVMBuildCondBr(builder, cond, then_block, else_block);
-
-        //then
-        LLVMPositionBuilderAtEnd(builder, then_block);
-        LLVMAppendExistingBasicBlock(llvm_func,  then_block);
-        _llvm_code_gen(if_stmt->if_body,  llvm_func,  mod,  ctx);
-        LLVMBuildBr(builder, cont_block);
-
-        //Else
-        if(if_stmt->else_body != NULL){
-            LLVMPositionBuilderAtEnd(builder, else_block);
-            LLVMAppendExistingBasicBlock(llvm_func,  else_block);
-            _llvm_code_gen(if_stmt->else_body,  llvm_func,  mod,  ctx);
-            LLVMBuildBr(builder, cont_block);
-        }
-        
-        LLVMAppendExistingBasicBlock(llvm_func,  cont_block);
-
+        if_stmt = (AstIfElse *)node->as;
+        llvm_gen_if(if_stmt, llvm_func, ctx);
         break;
     case AST_EMPTY_EXPR:
         break;
@@ -254,7 +306,7 @@ void llvm_func_def(AstFuncDef *func, LLVMModuleRef mod, LLVMContextRef ctx)
     LLVMValueRef llvm_func = LLVMAddFunction(mod, func->value, func_type);
     func->symbol->llvm_value_ref = llvm_func;
     func->symbol->llvm_type_ref = func_type;
-    _llvm_code_gen(func->body, llvm_func, mod, ctx);
+    _llvm_code_gen(func->body, llvm_func, ctx);
 }
 
 void save_module_to_file(LLVMModuleRef module, const char *filename)
--- src/llvm/backend.h
+++ src/llvm/backend.h
@@ -1,7 +1,11 @@
 #ifndef LLVM_BACKEND_H
 #define LLVM_BACKEND_H
 
+#include <mylang/ast.h>
 #include <mylang/util.h>
 void llvm_code_gen(Vector *prog);
 
+void _llvm_code_gen(AstNode *node, LLVMValueRef llvm_func, LLVMContextRef ctx);
+
+LLVMValueRef llvm_eval(AstNode *node, LLVMValueRef llvm_func, LLVMContextRef ctx);
 #endif
